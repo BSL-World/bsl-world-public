ab2adc1d673418537cdd41e0e14e7a43e6a56f79
Alexander (BSL) Vasilyev <soft@BSL-World.ru>
2026-08-24 19:33:17 +0300

    chore: remove BSL-Tor from public repository

--- bsl-tor/src-tauri/build.rs
+++
@@ -1,9 +0,0 @@
-fn main() {
-    if std::env::var("CARGO_CFG_TARGET_OS").unwrap() == "windows" {
-        std::env::set_var(
-            "TAURI_WINDOWS_EXECUTABLE_MANIFEST",
-            "requireAdministrator"
-        );
-    }
-    tauri_build::build()
-}
